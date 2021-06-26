8dcc07ce83dc159720ead7154ba23ef4f114852e
Roman Petrenko <roman@canva.com>
2021-06-26 10:18:32 +1000

    display the current figure

---
+++ Game.gd
@@ -0,0 +1,48 @@
+extends ColorRect
+
+onready var tile = preload('res://Tile.tscn')
+
+onready var figureView = $Field/Figure
+
+const tileSize = 20
+
+var field = [
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+var figure = [
+	[0, 1, 0, 0],
+	[0, 1, 1, 0],
+	[0, 0, 1, 0],
+	[0, 0, 0, 0]
+]
+var figurePosition = Vector2(5, 5)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	figureView.rect_position = Vector2(figurePosition.x * tileSize, figurePosition.y * tileSize)
+	for y in figure.size():
+		for x in figure[y].size():
+			if figure[y][x] == 1:
+				var newTile = tile.instance()
+				newTile.rect_position = Vector2(x * tileSize, y * tileSize)
+				figureView.add_child(newTile)
