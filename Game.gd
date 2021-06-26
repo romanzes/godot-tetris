c90e0234e69b4bb3f1c9e4ed7d65b618526c7f18
Roman Petrenko <roman@canva.com>
2021-06-26 12:15:58 +1000

    stop figure when it reaches bottom

--- Game.gd
+++ Game.gd
@@ -3,6 +3,7 @@ extends ColorRect
 onready var tile = preload('res://Tile.tscn')
 
 onready var figureView = $Field/Figure
+onready var tilesView = $Field/Tiles
 
 const tileSize = 20
 
@@ -35,11 +36,12 @@ var figure = [
 	[0, 0, 1, 0],
 	[0, 0, 0, 0]
 ]
-var figurePosition = Vector2(5, 5)
+const initialFigurePosition = Vector2(3, -4)
+var figurePosition = initialFigurePosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position_Figure_View()
+	update_Figure_View()
 	for y in figure.size():
 		for x in figure[y].size():
 			if figure[y][x] == 1:
@@ -50,7 +52,33 @@ func _ready():
 
 func _on_Timer_timeout():
 	figurePosition.y += 1
-	position_Figure_View()
+	check_Ground()
+	update_Figure_View()
 
-func position_Figure_View():
+func update_Figure_View():
 	figureView.rect_position = Vector2(figurePosition.x * tileSize, figurePosition.y * tileSize)
+
+func check_Ground():
+	for y in figure.size():
+		for x in figure[y].size():
+			if figure[y][x] == 1:
+				if figurePosition.y + y + 1 == field.size():
+					stop_Figure()
+
+func stop_Figure():
+	for y in figure.size():
+		for x in figure[y].size():
+			if figure[y][x] == 1:
+				field[figurePosition.y + y][figurePosition.x + x] = 1
+	update_Field_View()
+	figurePosition = initialFigurePosition
+
+func update_Field_View():
+	for n in tilesView.get_children():
+		n.free()
+	for y in field.size():
+		for x in field[y].size():
+			if field[y][x] == 1:
+				var newTile = tile.instance()
+				newTile.rect_position = Vector2(x * tileSize, y * tileSize)
+				tilesView.add_child(newTile)
