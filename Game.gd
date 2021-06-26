433cbc4241f1ddbf789361487401f5705375fdfa
Roman Petrenko <roman@canva.com>
2021-06-26 10:28:28 +1000

    move figure down every second

--- Game.gd
+++ Game.gd
@@ -39,10 +39,18 @@ var figurePosition = Vector2(5, 5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	figureView.rect_position = Vector2(figurePosition.x * tileSize, figurePosition.y * tileSize)
+	position_Figure_View()
 	for y in figure.size():
 		for x in figure[y].size():
 			if figure[y][x] == 1:
 				var newTile = tile.instance()
 				newTile.rect_position = Vector2(x * tileSize, y * tileSize)
 				figureView.add_child(newTile)
+	$Timer.start(1)
+
+func _on_Timer_timeout():
+	figurePosition.y += 1
+	position_Figure_View()
+
+func position_Figure_View():
+	figureView.rect_position = Vector2(figurePosition.x * tileSize, figurePosition.y * tileSize)
